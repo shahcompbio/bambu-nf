8dcf537e4af6f300f51c83e8f41110a82138b332
apsteinberg <asherps@gmail.com>
2025-05-20 17:39:41 -0400

    test samplesheet

---
+++ tests/APS033_bambu_nf.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+## activate nf-core conda environment
+source /home/preskaa/miniforge3/bin/activate nf-core
+
+module load java/20.0.1
+## specify params
+outdir=/data1/shahs3/users/preskaa/SarcAtlas/data/APS033_ont_transcript_assembly/bambu_nf_test/results
+pipelinedir=$HOME/bambu-nf
+samplesheet=${pipelinedir}/assets/samplesheet.csv
+
+mkdir -p ${outdir}
+cd ${outdir}
+
+nextflow run ${pipelinedir}/main.nf \
+    -profile local \
+    -work-dir ${outdir}/work \
+    --outdir ${outdir} \
+    --input ${samplesheet} \
+    -resume
