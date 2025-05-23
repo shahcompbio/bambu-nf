d34f163c6565627cad4a25fc727492faf5f2298d
Asher <preskaa@mskcc.org>
2025-05-23 16:21:52 -0400

    fix to bambu filter

--- bin/bambu_filter.R
+++ bin/bambu_filter.R
@@ -4,14 +4,16 @@ library(optparse)
 ## load in commandline args
 option_list = list(
   make_option("--se", type="character", default=NULL, 
-              help="bambu read classes", metavar="character")
+              help="bambu read classes", metavar="character"),
+  make_option("--n_samples", type="integer", default=1, 
+              help="number of samples", metavar="character")
 );
 opt_parser = OptionParser(option_list=option_list);
 opt = parse_args(opt_parser);
 # load summarized experiment object
-load(se)
+load(opt$se)
 # filter on full-length counts
-if (n_samples > 1) {
+if (opt$n_samples > 1) {
   fullLengthCounts <- assays(se)$fullLengthCounts
   rows_satisfying_conditions <- apply(fullLengthCounts, 1, function(row) any(row > 1))
   # Subset the SummarizedExperiment object based on the condition
@@ -20,4 +22,4 @@ if (n_samples > 1) {
   se.detected <- se[assays(se)$fullLengthCounts > 1,]
 }
 detectgtf <- rowRanges(se.detected)
-writeToGTF(detectgtf, detectpath)
+writeToGTF(detectgtf, "detected_transcripts.gtf")
