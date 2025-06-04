b30e1592057e804a3aa9a2949a859aede22ccfed
apsteinberg <asherps@gmail.com>
2025-06-04 15:42:44 -0400

    subworkflow for preprocessing reads

--- tests/APS033_bambu_nf.sh
+++ tests/APS033_bambu_nf.sh
@@ -15,7 +15,7 @@ source /home/preskaa/miniforge3/bin/activate nf-core
 
 module load java/20.0.1
 ## specify params
-outdir=/data1/shahs3/users/preskaa/SarcAtlas/data/APS033_ont_transcript_assembly/bambu_nf_test/multisample_results
+outdir=/data1/shahs3/users/preskaa/SarcAtlas/data/APS033_ont_transcript_assembly/bambu_nf_test/preprocess_test
 pipelinedir=$HOME/bambu-nf
 samplesheet=${pipelinedir}/assets/samplesheet.csv
 fasta=/data1/shahs3/isabl_data_lake/assemblies/GRCh38-P14/GRCh38.primary_assembly.genome.fa
